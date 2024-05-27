ac3698d172648773d5718564e0f4d4c9d50e0f3a
Nafe <121440579+Nafe-Ibne-Dalower@users.noreply.github.com>
2024-05-27 07:42:05 +0600

    Update Chapter-30 Creating Virtual Environment.py

--- Chapter-30 Creating Virtual Environment.py
+++ Chapter-30 Creating Virtual Environment.py
@@ -4,9 +4,9 @@ print("Hello World!😉😉😉")
 1. Go to the folder.
 2. shift + left key > Open Powershell Admin
 3. pip install virtualenv
-4. python -3 -m venv _name_
-5. python -3 -m venv activate
-6. python -3 -m venv deactivate
+4. python -m venv _name_
+5. python -m venv activate
+6. python -m venv deactivate
 7. pip freeze > requirments.txt
 8. pip install -r requierments.txt
 """
